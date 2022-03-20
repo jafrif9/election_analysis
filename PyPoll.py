30d06326a3dda893523fc73fd1a9137630c9a4ef
jafrif9 <86126331+jafrif9@users.noreply.github.com>
2022-03-20 16:45:52 -0400

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -16,6 +16,11 @@ candidate_options = []
 # Declare the empty dictionary
 candidate_votes = {}
 
+# Winning candidate and winning count tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
 
@@ -53,13 +58,28 @@ for candidate_name in candidate_votes:
     # Calculate the percentage of votes
     vote_percentage = float(votes) / float(total_votes) * 100
 
-    # Print the candidate name and percentage of votes
-    print(f"{candidate_name}: received {vote_percentage:.1f}% of the vote.")
+    # Print out each candidate's name, vote count, and percentage of votes on the terminal
+    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-# Print the total votes.
-print(total_votes)
-print(candidate_options)
-print(candidate_votes)
+    # Determine winning vote count and candidate
+    # Determine if the cotes are greater than the winning count
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        # If true then set winning_count = votes and winning_percent = vote_percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        # set the winning_candidate to the candidate's name
+        winning_candidate = candidate_name
+
+
+# Print winning candidate information
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+
+print(winning_candidate_summary)
 
 # The data we need to retrieve
 # 1. The total numbe rof votes cast
